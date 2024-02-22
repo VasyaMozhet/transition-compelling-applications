67020e371f7c2fd860446a146c74eb3f74b8c7b3
VasyaMozhet <158567931+VasyaMozhet@users.noreply.github.com>
2024-02-22 19:53:41 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,39 +1,13 @@
-        print(f"Random Number: {item}")
+def main():
+    for item in data:
 def generate_random_data():
-if __name__ == "__main__":
 
-
-    data = [random.randint(1, 100) for _ in range(10)]
     main()
-import random
-
     data = generate_random_data()
     return data
-
-def main():
-    for item in data:    data = generate_random_data()
-import random
-if __name__ == "__main__":
-    return data
-
-def main():
-
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-
-    main()
         print(f"Random Number: {item}")
-    for item in data:
-        print(f"Random Number: {item}")
-    main()
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
 import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
 if __name__ == "__main__":
-    return data
-
-def main():
-
-def generate_random_data():
-    for item in data:
 
